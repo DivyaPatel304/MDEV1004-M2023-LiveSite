12bf963189799c5c1ffcc2ac67818c21dd2af799
DivyaPatel304 <200538095@student.georgianc.on.ca>
2023-06-11 10:03:52 -0400

    Folder deleted

--- Server/Config/app.ts
+++ Server/Config/app.ts
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.json()); 
-app.use(express.static(path.join(__dirname, '../../Client')));
+
 
 app.use('/', indexRouter);
 
