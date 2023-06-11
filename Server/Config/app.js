151c91dab81aa2dd0d490777431da2592efddf6c
DivyaPatel304 <200538095@student.georgianc.on.ca>
2023-06-11 10:20:35 -0400

    Changed to Remote - TSBUILD

--- Server/Config/app.js
+++ Server/Config/app.js
@@ -4,14 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const path_1 = __importDefault(require("path"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const morgan_1 = __importDefault(require("morgan"));
 const index_1 = __importDefault(require("../Routes/index"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const db_1 = __importDefault(require("./db"));
-mongoose_1.default.connect(db_1.default.localURI);
+mongoose_1.default.connect(db_1.default.remoteURI);
 mongoose_1.default.connection.on('connected', () => {
     console.log(`Connected to MongoDB`);
 });
@@ -24,7 +23,6 @@ app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use((0, cookie_parser_1.default)());
 app.use(body_parser_1.default.json());
-app.use(express_1.default.static(path_1.default.join(__dirname, '../../Client')));
 app.use('/', index_1.default);
 exports.default = app;
 //# sourceMappingURL=app.js.map
